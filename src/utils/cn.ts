fd79ebd24002499d23e01d5afab6aa11ba4ae86a
michu2k <michal.strumpf@gmail.com>
2024-11-15 19:55:39 +0100

    Added cn helper

---
+++ src/utils/cn.ts
@@ -0,0 +1,8 @@
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+function cn(...classes: Array<ClassValue>) {
+  return twMerge(clsx(classes));
+}
+
+export {cn};
