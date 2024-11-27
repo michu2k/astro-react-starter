fa61242542685a77cc4ed7acc66634adcb7435f1
michu2k <michal.strumpf@gmail.com>
2024-11-27 18:52:45 +0100

    Adjusted name and button text

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -3,21 +3,21 @@ import {useState} from "react";
 const Example = () => {
   const [isVisible, setIsVisible] = useState(true);
 
-  function toggleName() {
+  function toggleText() {
     setIsVisible(!isVisible);
   }
 
   return (
-    <div className="mt-8 flex w-60 flex-col items-center gap-3 text-foreground">
+    <div className="mt-8 flex flex-col items-center gap-3 text-foreground">
       <button
-        onClick={toggleName}
-        className="bg-muted hover:bg-muted/75 rounded-md px-4 py-2 font-semibold transition-colors">
-        Toggle name
+        onClick={toggleText}
+        className="bg-muted hover:bg-muted/75 rounded-full px-4 py-2 font-semibold transition-colors">
+        Toggle text
       </button>
 
-      <p className="flex items-center gap-1 text-center text-lg">
-        Example
-        <strong className={`w-12 transition-opacity ${isVisible ? "opacity-100" : "opacity-0"}`}>React</strong>
+      <p className="text-center text-lg">
+        An example
+        <strong className={`mx-2 w-12 transition-opacity ${isVisible ? "opacity-100" : "opacity-0"}`}>React</strong>
         component
       </p>
     </div>
