c3c6beff4c3f9d21ea1381a8d2e7d8cbf355ef6c
michu2k <michal.strumpf@gmail.com>
2025-05-04 13:48:07 +0200

    Renamed components

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -11,7 +11,7 @@ const Example = () => {
     <div className="text-foreground mt-8 flex flex-col items-center gap-4">
       <button
         onClick={toggleText}
-        className="bg-muted hover:bg-muted/75 cursor-pointer rounded-full px-4 py-2 font-semibold transition-colors">
+        className="bg-muted hover:bg-muted/75 cursor-pointer rounded-lg px-4 py-2 font-semibold transition-colors">
         Toggle text
       </button>
 
